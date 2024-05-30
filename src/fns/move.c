011d6d62fdce91e83036708e6158eb5ac4c49c05
ivalimak <ivalimak@student.hive.fi>
2024-05-30 14:39:42 +0300

    Added more movement commands Added backward-word Added forward-word Added clear-screen

--- src/fns/move.c
+++ src/fns/move.c
@@ -6,11 +6,12 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 02:11:04 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/05/30 12:13:52 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/05/30 14:38:49 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rl_internal.h"
+#include "ft_stdio/ft_printf.h"
 
 uint8_t	ft_rl_sol(rl_input_t *input)
 {
@@ -68,18 +69,48 @@ uint8_t	ft_rl_bck(rl_input_t *input)
 
 uint8_t	ft_rl_fwd_w(rl_input_t *input)
 {
+	if (input->i == input->len)
+		return (1);
+	ft_rl_word_end();
+	input->i++;
+	while (input->i < input->len && ft_isspace(input->line[input->i]))
+		input->i++;
+	input->cursor->row = input->cursor->i_row;
+	input->cursor->col = input->cursor->i_col + input->i;
+	ft_rl_cursor_setpos(input->cursor);
 	return (1);
-	(void)input;
 }
 
 uint8_t	ft_rl_bck_w(rl_input_t *input)
 {
+	uint64_t	i;
+
+	if (input->i == 0)
+		return (1);
+	i = input->i;
+	ft_rl_word_start();
+	if (input->i == i)
+	{
+		input->i--;
+		while (input->i > 0 && ft_isspace(input->line[input->i]))
+			input->i--;
+		ft_rl_word_start();
+	}
+	input->cursor->row = input->cursor->i_row;
+	input->cursor->col = input->cursor->i_col + input->i;
+	ft_rl_cursor_setpos(input->cursor);
 	return (1);
-	(void)input;
 }
 
 uint8_t	ft_rl_clr(rl_input_t *input)
 {
+	input->cursor->p_row = 1;
+	input->cursor->p_col = 1;
+	ft_printf("%s%s%s", TERM_CUR_RESET, TERM_CLEAR_END, input->prompt);
+	ft_rl_cursor_getpos(&input->cursor->i_row, &input->cursor->i_col);
+	ft_putstr_fd(input->line, 1);
+	input->cursor->row = input->cursor->i_row;
+	input->cursor->col = input->cursor->i_col + input->i;
+	ft_rl_cursor_setpos(input->cursor);
 	return (1);
-	(void)input;
 }
