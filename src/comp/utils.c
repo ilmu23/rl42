e6038a419a9047fe33d4654e21f53cb2ea97bbc2
ivalimak <ivalimak@student.hive.fi>
2024-06-03 17:24:17 +0300

    Fixed segfault

--- src/comp/utils.c
+++ src/comp/utils.c
@@ -6,14 +6,12 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 13:55:57 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/06/03 15:40:05 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:24:06 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rl_internal.h"
 #include "ft_stdio/ft_printf.h"
-
-static inline uint64_t	_gettotallen(const t_list *completions);
 
 uint64_t	ft_rl_comp_getlongest(const t_list *completions)
 {
@@ -31,6 +29,7 @@ uint64_t	ft_rl_comp_getlongest(const t_list *completions)
 	return (longest);
 }
 
+// disgusting
 void	ft_rl_comp_display(rl_input_t *input, const t_list *completions, const void *cur)
 {
 	uint16_t	i;
@@ -38,6 +37,7 @@ void	ft_rl_comp_display(rl_input_t *input, const t_list *completions, const void
 	uint16_t	rows;
 	uint64_t	llen;
 	uint64_t	asize;
+	uint64_t	cslen;
 	char		*cstr;
 
 	llen = ft_rl_comp_getlongest(completions);
@@ -51,35 +51,34 @@ void	ft_rl_comp_display(rl_input_t *input, const t_list *completions, const void
 	}
 	ft_rl_redisplay(input, PROMPT);
 	i = 0;
-	asize = _gettotallen(completions) + rows + ft_strlen(ft_rl_hlcolor()) + ft_strlen(SGR_RESET) + 1;
+	cslen = 0;
+	asize = (*completions->size * llen) + rows + ft_strlen(ft_rl_hlcolor()) + ft_strlen(SGR_RESET) + 1;
 	cstr = ft_push(ft_calloc(asize, sizeof(*cstr)));
 	while (completions)
 	{
-		if (completions->blk != cur)
-			ft_snprintf(&cstr[ft_strlen(cstr)], asize, "%*s ", llen, completions->blk);
-		else
-			ft_snprintf(&cstr[ft_strlen(cstr)], asize, "%s%*s%s ", ft_rl_hlcolor(), llen, completions->blk, SGR_RESET);
-		i++;
-		if (i == cpr)
+		if (completions->blk == cur)
 		{
-			ft_sprintf(&cstr[ft_strlen(cstr)], "\n");
+			ft_memcpy(cstr + cslen, ft_rl_hlcolor(), ft_strlen(ft_rl_hlcolor()));
+			cslen = ft_strlen(cstr);
+		}
+		ft_memset(cstr + cslen, ' ', llen - ft_strlen(completions->blk));
+		cslen += llen - ft_strlen(completions->blk);
+		ft_memcpy(cstr + cslen, completions->blk, ft_getblksize(completions->blk));
+		cslen = ft_strlen(cstr);
+		if (completions->blk == cur)
+		{
+			ft_memcpy(cstr + cslen, SGR_RESET, ft_strlen(SGR_RESET));
+			cslen = ft_strlen(cstr);
+		}
+		if (++i == cpr)
+		{
+			ft_memcpy(cstr + cslen++, "\n", 1);
 			i = 0;
 		}
+		else
+			ft_memcpy(cstr + cslen++, " ", 1);
 		completions = completions->next;
 	}
 	ft_printf("\n%s", cstr);
 	ft_rl_cursor_reset(input);
-}
-
-static inline uint64_t	_gettotallen(const t_list *completions)
-{
-	uint64_t	len;
-
-	len = 0;
-	while (completions)
-	{
-		len += ft_strlen(completions->blk);
-		completions = completions->next;
-	}
-	return (len);
 }
