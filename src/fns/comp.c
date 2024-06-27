26f4486b72b5f14f263e94f04cea60d1259e780b
ivalimak <ivalimak@student.hive.fi>
2024-06-27 19:13:24 +0300

    completion-ignore-case and completion-map-case are now handled

--- src/fns/comp.c
+++ src/fns/comp.c
@@ -6,7 +6,7 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 02:16:07 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/06/27 15:09:01 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/06/27 18:49:45 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ uint8_t	ft_rl_cmp(rl_input_t *input)
 {
 	if (input->len == 0 || !ft_rl_get(_CMP_ENABLE_HASH))
 		return (1);
-	input->key = 0;
 	ft_rl_complete(input);
 	if (ft_rl_ismapped(input->key) && ft_rl_getmap(input->key) != ft_rl_cmp)
 	{
