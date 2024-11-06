ab241a4af057b18d5e220256447e08024d8c87a4
ivalimak <ivalimak@student.hive.fi>
2024-11-06 14:45:09 +0200

    Added NULL check to ft_rl_cmp

--- src/fns/comp.c
+++ src/fns/comp.c
@@ -6,7 +6,7 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 02:16:07 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/10/30 20:29:26 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:44:15 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ uint8_t	ft_rl_cmp(rl_input_t *input)
 	if (input->len == 0 || !ft_rl_get(_CMP_ENABLE_HASH))
 		return (1);
 	f = ft_rl_complete(input);
-	return (f != ft_rl_cmp) ? f(input) : 1;
+	return (f && f != ft_rl_cmp) ? f(input) : 1;
 }
